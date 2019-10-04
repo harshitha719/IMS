cfbd471ad5b809b58aef42d9c0854749aaab314c
harshitha719 <55902125+harshitha719@users.noreply.github.com>
2019-10-03 22:52:51 -0500

    Add files via upload
    constant file updated

--- IMS/InventoryManagementSystem/src/Constant/IMSConstants.java
+++ IMS/InventoryManagementSystem/src/Constant/IMSConstants.java
@@ -1,25 +1,27 @@
 package Constant;
 
-
-
-public  class IMSConstants {
-public final String  HARDWARE = "Hardware";
-public final String  ONE = "1";
-public final String  TWO = "2";
-public final String  THREE = "3";
-public final String  FOUR = "4";
-public final String  FIVE = "5";
-public final String  SIX = "6";
-public final String  ZERO = "0";
-public final String  TWODOTONE = "2.1";
-public final String  TWODOTTWO = "2.2";
-public final String  TWODOTTHREE = "2.3";
-public final String ENTERPRODUCT ="Enter Product or ProductID to search:";
-public final String ENTERMAINMENU ="Enter the Menu Number:";
-public final String ERRORINVALIDMENU2 ="Please select valid number from Menu2";
-public final String ERRORPRODUCTNOTPRESENT ="productID or Product not present";
-public final String ENTERDATAFILE ="Enter Data File Name";
-public final String TXT =".txt";
-public final String ERRORINVALIDFILE = "Invalid Data File";
+public final class IMSConstants {
+	private IMSConstants() {
+        // restricting instantiation
+}
+public static final String  HARDWARE = "Hardware";
+public static final String  STATIONARY = "STATIONARY";
+public static final String  ONE = "1";
+public static final String  TWO = "2";
+public static final String  THREE = "3";
+public static final String  FOUR = "4";
+public static final String  FIVE = "5";
+public static final String  SIX = "6";
+public static final String  ZERO = "0";
+public static final String  TWODOTONE = "2.1";
+public static final String  TWODOTTWO = "2.2";
+public static final String  TWODOTTHREE = "2.3";
+public static final String ENTERPRODUCT ="Enter Product or ProductID to search:";
+public static final String ENTERMAINMENU ="Enter the Menu Number:";
+public static final String ERRORINVALIDMENU2 ="Please select valid number from Menu2 \n";
+public static final String ERRORPRODUCTNOTPRESENT ="productID or Product not present \n";
+public static final String ENTERDATAFILE ="Enter Data File Name";
+public static final String TXT =".txt";
+public static final String ERRORINVALIDFILE = "Invalid Data File \n";
 
 }
